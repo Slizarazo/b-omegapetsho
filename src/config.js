b6c97c15ac53e10280cedbae93fcc9d24e48e1cc
Sebastian Lizarazo <sebastian.lizarazo@outlook.es>
2022-11-16 14:34:36 -0500

    correccion de error en config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     api: {
         // baseURL: "http://localhost:4000"
-        basURL: "https://b-omegapetshop-g05.herokuapp.com"
+        baseURL: "https://b-omegapetshop-g05.herokuapp.com"
     }
 }
